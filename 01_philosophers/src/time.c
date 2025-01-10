15b30dbbc41e1a8b4973212613416a8eceb34ea3
Julie Jeannine Hunter Franco <jfranco@shi-r6-p5.s19.be>
2025-01-10 19:17:43 +0100

    DONT_WORK

--- 01_philosophers/src/time.c
+++ 01_philosophers/src/time.c
@@ -6,7 +6,7 @@
 /*   By: jfranco <jfranco@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 17:20:51 by jfranco           #+#    #+#             */
-/*   Updated: 2025/01/10 17:44:16 by jfranco          ###   ########.fr       */
+/*   Updated: 2025/01/10 19:12:37 by jfranco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	get_time_stmp(t_data *data)
 	long long	stamp;
 
 	gettimeofday(&start, NULL);
-	time_start_mille = start.tv_sec * 1001 + start.tv_usec / 1000;
+	time_start_mille = start.tv_sec * 1000 + start.tv_usec / 1000;
 	gettimeofday(&now, NULL);
 	time_now_mille = now.tv_sec * 1000 + now.tv_usec / 1000;
 	stamp = time_start_mille - time_now_mille;
